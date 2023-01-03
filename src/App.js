973c7be2f2131932386991da2447245c9ef3030f
Juan Pablo Montoya <101369659+jmontoyavallejo@users.noreply.github.com>
2023-01-03 16:45:50 -0500

    first comit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import images from "./imagenes/images.jpg";
+import Tarea from './componentes/Tarea/Tarea'
+import TareaFormulario from './componentes/TareaFormulario/TareaFormulario'
+import ListaDeTareas from './componentes/ListaDeTareas/ListaDeTareas'
+
+
+
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          src={images}
+          alt="logo de freecodecamp"
+          className="freecodecamp-logo"
+        />
+      </div>
+      <div className="tareas-lista-principal">
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
+}
+
+export default App;
